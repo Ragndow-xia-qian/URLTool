255c7f2206a79b89555ad3790c836b37e2f77bd2
Ragndow-xia-qian <372378529@qq.com>
2023-11-22 22:59:16 +0800

    Optimize details

---
+++ cmake-build-debug/CMakeFiles/OpenURLTool.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Code/cpp/Qt5 Projects/OpenURLTool/cmake-build-debug/OpenURLTool_autogen/mocs_compilation.cpp" "CMakeFiles/OpenURLTool.dir/OpenURLTool_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/OpenURLTool.dir/OpenURLTool_autogen/mocs_compilation.cpp.obj.d"
+  "D:/Code/cpp/Qt5 Projects/OpenURLTool/Sources/Module/AskPopup.cpp" "CMakeFiles/OpenURLTool.dir/Sources/Module/AskPopup.cpp.obj" "gcc" "CMakeFiles/OpenURLTool.dir/Sources/Module/AskPopup.cpp.obj.d"
+  "D:/Code/cpp/Qt5 Projects/OpenURLTool/Sources/Module/BrowserDiaLog.cpp" "CMakeFiles/OpenURLTool.dir/Sources/Module/BrowserDiaLog.cpp.obj" "gcc" "CMakeFiles/OpenURLTool.dir/Sources/Module/BrowserDiaLog.cpp.obj.d"
+  "D:/Code/cpp/Qt5 Projects/OpenURLTool/Sources/Module/ErrorPopup.cpp" "CMakeFiles/OpenURLTool.dir/Sources/Module/ErrorPopup.cpp.obj" "gcc" "CMakeFiles/OpenURLTool.dir/Sources/Module/ErrorPopup.cpp.obj.d"
+  "D:/Code/cpp/Qt5 Projects/OpenURLTool/Sources/Module/InfoPopup.cpp" "CMakeFiles/OpenURLTool.dir/Sources/Module/InfoPopup.cpp.obj" "gcc" "CMakeFiles/OpenURLTool.dir/Sources/Module/InfoPopup.cpp.obj.d"
+  "D:/Code/cpp/Qt5 Projects/OpenURLTool/Sources/Module/InputListDiaLog.cpp" "CMakeFiles/OpenURLTool.dir/Sources/Module/InputListDiaLog.cpp.obj" "gcc" "CMakeFiles/OpenURLTool.dir/Sources/Module/InputListDiaLog.cpp.obj.d"
+  "D:/Code/cpp/Qt5 Projects/OpenURLTool/Sources/Module/WarningPopup.cpp" "CMakeFiles/OpenURLTool.dir/Sources/Module/WarningPopup.cpp.obj" "gcc" "CMakeFiles/OpenURLTool.dir/Sources/Module/WarningPopup.cpp.obj.d"
+  "D:/Code/cpp/Qt5 Projects/OpenURLTool/Sources/Module/mainwindow.cpp" "CMakeFiles/OpenURLTool.dir/Sources/Module/mainwindow.cpp.obj" "gcc" "CMakeFiles/OpenURLTool.dir/Sources/Module/mainwindow.cpp.obj.d"
+  "D:/Code/cpp/Qt5 Projects/OpenURLTool/Sources/main.cpp" "CMakeFiles/OpenURLTool.dir/Sources/main.cpp.obj" "gcc" "CMakeFiles/OpenURLTool.dir/Sources/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OpenURLTool.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenURLTool.dir/OpenURLTool_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/OpenURLTool.dir/OpenURLTool_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/OpenURLTool.dir/Sources/Module/AskPopup.cpp.obj"
+  "CMakeFiles/OpenURLTool.dir/Sources/Module/AskPopup.cpp.obj.d"
+  "CMakeFiles/OpenURLTool.dir/Sources/Module/BrowserDiaLog.cpp.obj"
+  "CMakeFiles/OpenURLTool.dir/Sources/Module/BrowserDiaLog.cpp.obj.d"
+  "CMakeFiles/OpenURLTool.dir/Sources/Module/ErrorPopup.cpp.obj"
+  "CMakeFiles/OpenURLTool.dir/Sources/Module/ErrorPopup.cpp.obj.d"
+  "CMakeFiles/OpenURLTool.dir/Sources/Module/InfoPopup.cpp.obj"
+  "CMakeFiles/OpenURLTool.dir/Sources/Module/InfoPopup.cpp.obj.d"
+  "CMakeFiles/OpenURLTool.dir/Sources/Module/InputListDiaLog.cpp.obj"
+  "CMakeFiles/OpenURLTool.dir/Sources/Module/InputListDiaLog.cpp.obj.d"
+  "CMakeFiles/OpenURLTool.dir/Sources/Module/WarningPopup.cpp.obj"
+  "CMakeFiles/OpenURLTool.dir/Sources/Module/WarningPopup.cpp.obj.d"
+  "CMakeFiles/OpenURLTool.dir/Sources/Module/mainwindow.cpp.obj"
+  "CMakeFiles/OpenURLTool.dir/Sources/Module/mainwindow.cpp.obj.d"
+  "CMakeFiles/OpenURLTool.dir/Sources/main.cpp.obj"
+  "CMakeFiles/OpenURLTool.dir/Sources/main.cpp.obj.d"
+  "D:/Code/cpp/Qt5 Projects/OpenURLTool/bin/OpenURLTool.exe"
+  "D:/Code/cpp/Qt5 Projects/OpenURLTool/bin/OpenURLTool.exe.manifest"
+  "D:/Code/cpp/Qt5 Projects/OpenURLTool/bin/OpenURLTool.pdb"
+  "D:/Code/cpp/Qt5 Projects/OpenURLTool/bin/libOpenURLTool.dll.a"
+  "CMakeFiles/OpenURLTool_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/OpenURLTool_autogen.dir/ParseCache.txt"
+  "OpenURLTool_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenURLTool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
